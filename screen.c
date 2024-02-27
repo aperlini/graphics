667ec9b7309db5408f52cdc05af2327f58bf8af8
Adriano Perlini <aperlini@protonmail.ch>
2024-02-27 21:46:40 +0100

    getting rid of pointer

--- screen.c
+++ screen.c
@@ -1,46 +1,33 @@
 #include "screen.h"
 
 // Global screen infos
-static screen *s = NULL;
+static screen s = {0};
 
 void init(int w, int h, color fg, color bg) {
 
-	// Persists infos
-	s = (screen*)(malloc(sizeof(screen)));
-
-	if(s == NULL) {
-		fprintf(stderr, "Issue w/ malloc!\n");
-		exit(-1);
-	}
-
-	s->w = w;
-	s->h = h;
-	s->bg = bg;
-	s->fg = fg;
+	s.w = w;
+	s.h = h;
+	s.bg = bg;
+	s.fg = fg;
 
 	// Open a new window for drawing.
-	gfx_open(s->w, s->h,"Default cross", s->fg, s->bg);
+	gfx_open(s.w, s.h,"Default cross", s.fg, s.bg);
 }
 
 int screen_width() {
-	return s->w;
+	return s.w;
 }
 
 int screen_height() {
-	return s->h;
+	return s.h;
 }
 
 color foreground() {
-	return s->fg;
+	return s.fg;
 }
 
 color background() {
-	return s->bg;
-}
-
-void clean_up() {
-	free(s);
-	s = NULL;
+	return s.bg;
 }
 
 void enable_display() {
